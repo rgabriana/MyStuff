8e223dbbcf538d3ed90ba11d6853e90753f0f3a5
RRG <rolando.gabriana@gmail.com>
2017-03-01 09:00:47 -0800

    selinux prompt changes
    Added a function to change the prompt and color.

--- config/etc/profile.d/selinux_prompt.sh
+++ config/etc/profile.d/selinux_prompt.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
-
-red='\[\033[0;31m\]'
-green='\[\033[0;32m\]'
-blue='\[\033[0;34m\]'
-nocolor='\[\033[0m\]'
-
-case $(getenforce) in 
-  Permissive)
-  color=$red
-  ;;
-
-  Enforcing)
-  color=$green
-  ;;
-
-  *)
-  color=$blue
-  ;;
-esac
+# Simple script that changes the users prompt dependent on the SELINUX status
 
 if  [ "$PS1" ]; then 
-  PS1="[\u@\h:$color\$(getenforce)$nocolor \W]\\$ "
+  selogo () {
+    red='\[\033[0;31m\]'
+    green='\[\033[0;32m\]'
+    blue='\[\033[0;34m\]'
+    nocolor='\[\033[0m\]'
+
+      case $(getenforce) in 
+        Permissive)
+          color=$redPermissive$nocolor
+        ;;
+
+        Enforcing)
+          color=$greenEnforcing$nocolor
+        ;;
+
+        *)
+          color=$blueUNKNOWN$nocolor
+        ;;
+    
+      esac
+  
+  }
+
+  PS1="[\u@\h:$(selogo) \W]\\$ "
+
 fi
