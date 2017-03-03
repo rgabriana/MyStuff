97762c9d066e9947bfc946305c1b21b32ad7f206
RRG <rolando.gabriana@gmail.com>
2017-03-02 17:13:11 -0800

    Updated selinux_prompt.sh
    Corrected the bash grammar and added a prompt instead of trying to align with the existing one.

--- config/etc/profile.d/selinux_prompt.sh
+++ config/etc/profile.d/selinux_prompt.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
 # Simple script that changes the users prompt dependent on the SELINUX status
 
-if  [ "$PS1" ]; then 
-  selogo () {
-    red='\[\033[0;31m\]'
-    green='\[\033[0;32m\]'
-    blue='\[\033[0;34m\]'
-    nocolor='\[\033[0m\]'
+if  [ "$PS1" ]; then
 
-      case $(getenforce) in 
+  # color codes
+    red='\033[01;31m'
+    green='\033[01;32m'
+    blue='\033[00;34m'
+    nocolor='\033[0m'
+    
+  selogo ()
+
+  {
+      case $(getenforce) in
+      
         Permissive)
-          color=$redPermissive$nocolor
+          color="$red"$(getenforce)"$nocolor"
         ;;
 
         Enforcing)
-          color=$greenEnforcing$nocolor
+          color="$green"$(getenforce)"$nocolor"
         ;;
 
         *)
-          color=$blueUNKNOWN$nocolor
+          color="$blue"$(getenforce)"$nocolor"
         ;;
     
-      esac
-  
+       esac
+
+      echo -ne $color
   }
 
-  PS1="[\u@\h:$(selogo) \W]\\$ "
-
+    # show the prompt
+    PS1='[SELinux: $(status)]\n[\u@\h: \W]\$ '
+    
 fi
